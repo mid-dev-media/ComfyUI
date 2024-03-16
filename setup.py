8f0772bc8e977b0b810fd84015b83569cd10ac45
MiD <mid@mid.dev>
2024-03-16 01:01:11 -0700

    testing config

--- setup.py
+++ setup.py
@@ -11,38 +11,32 @@ with open(config_file) as f:
     config = json.load(f)
 
 # Process the "models" section
-models = config.get('models', [])
-for each_model_type in models:
-    folder = each_model_type.get('folder')
-    files = each_model_type.get('files')
+for each_folder in config.get('wget', []):
+    folder = each_folder.get('folder')
+    urls = each_folder.get('urls')
     os.chdir(os.path.join(current_dir, folder))
-    for each_file in files:
-        getter = each_file.get('getter')
-        url = each_file.get('url')
-        # Get the file name from the URL
+    for url in urls:
         file_name = url.split('/')[-1]
         # If the file does not exists in current directory
         if not os.path.exists(file_name):
             # Get the file
-            subprocess.run(f"{getter} {url}", shell=True)
+            subprocess.run(f"wget {url}", shell=True)
         else:
-            print(f"File {file_name} already exists")
+            print(f"{folder} {file_name} already exists")
 
 # Process the "nodes" section
-nodes = config.get('nodes', [])
-for each_node_type in nodes:
-    folder = each_node_type.get('folder')
-    files = each_node_type.get('files')
-    os.chdir(os.path.join(current_dir, folder))
-    print(f"Current directory: {os.getcwd()}")
-    for each_file in files:
-        getter = each_file.get('getter')
-        url = each_file.get('url')
+for each_repo in config.get('git', []):
+    folder = each_repo.get('folder')
+    urls = each_repo.get('urls')
+    os.chdir(current_dir)
+    for url in urls:
         # Get the file name from the URL
-        repo_folder_name = url.split('/')[-1].split('.')[0]
+        repo_name = url.split('/')[-1].split('.')[0]
         # If the file does not exists in current directory
-        if not os.path.exists(repo_folder_name):
+        if not os.path.exists(repo_name):
+            # Add the remote repository
+            subprocess.run(f"git remote add -f {repo_name} {url}", shell=True)
             # Get the file
-            subprocess.run(f"{getter} {url}", shell=True)
+            subprocess.run(f"git subtree add --prefix {folder}/{repo_name} {url} main", shell=True)
         else:
-            print(f"File {repo_folder_name} already exists")
+            print(f"{folder} {repo_name} already exists")
