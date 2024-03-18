122da1391602a65a13be9b0e37287aa9d9778772
MiD <mid@mid.dev>
2024-03-18 15:12:01 -0700

    Handle civitai links without file name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import os
 import json
 import subprocess
-
+import pprint
 # Get the path of the current directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -10,19 +10,40 @@ config_file = os.path.join(current_dir, 'setup.json')
 with open(config_file) as f:
     config = json.load(f)
 
+update_config = []
 # Process the "models" section
 for each_folder in config.get('wget', []):
     folder = each_folder.get('folder')
     urls = each_folder.get('urls')
     os.chdir(os.path.join(current_dir, folder))
     for url in urls:
-        file_name = url.split('/')[-1]
-        # If the file does not exists in current directory
-        if not os.path.exists(file_name):
-            # Get the file
-            subprocess.run(f"wget {url}", shell=True)
-        else:
-            print(f"{folder} {file_name} already exists")
+        if isinstance(url, str):
+            # Check for and handle civitai links that dont have filenames
+            if 'civitai' in url:
+                files_before = os.listdir(os.getcwd())
+                subprocess.run(f"wget {url} --content-disposition", shell=True)
+                files_after = os.listdir(os.getcwd())
+                new_files = list(set(files_after) - set(files_before))
+                print(new_files)
+                if len(new_files) == 1:
+                    file_name = new_files[0]
+                    update_config.append({'org': url, 'new':{'file_name': file_name, 'link': url}})
+                else:
+                    print(f"Error: Multiple files downloaded for {url}")
+            else:
+                file_name = url.split('/')[-1]
+                if not os.path.exists(file_name):
+                    subprocess.run(f"wget {url}", shell=True)
+                else:
+                    print(f"{folder} {file_name} already exists")
+
+        elif isinstance(url, dict):
+            file_name = url.get('file_name')
+            url = url.get('link')
+            if not os.path.exists(file_name):
+               subprocess.run(f"wget {url} --content-disposition", shell=True)
+            else:
+                print(f"{folder} {file_name} already exists")
 
 # Process the "nodes" section
 for each_repo in config.get('git', []):
@@ -40,3 +61,16 @@ for each_repo in config.get('git', []):
             subprocess.run(f"git subtree add --prefix {folder}/{repo_name} {url} main", shell=True)
         else:
             print(f"{folder} {repo_name} already exists")
+
+if update_config:
+    update = config.copy()
+    for each_folder in update.get('wget', []):
+        folder = each_folder.get('folder')
+        urls = each_folder.get('urls')
+        for i, each_urls in enumerate(urls):
+            for update_item in update_config:
+                if each_urls == update_item['org']:
+                    urls[i] = update_item['new']
+
+    with open(config_file, 'w') as f:
+        json.dump(update, f, indent=4)
